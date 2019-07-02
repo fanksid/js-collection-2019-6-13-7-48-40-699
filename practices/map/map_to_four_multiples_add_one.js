f4f0b0a27bef86321fbb3c569d302630dbb4ec0d
Kong Fanyue <fykong@thoughtworks.com>
2019-07-02 12:30:00 +0800

    Finish map to multiply with map

--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -1,6 +1,8 @@
 'use strict';
-var map_to_four_multiples_add_one = function(collection){
-  return [5,9,13,17,21];
+var map_to_four_multiples_add_one = function (collection) {
+  return collection.map(function (params) {
+    return params * 4 + 1;
+  });
 };
 
 module.exports = map_to_four_multiples_add_one;
