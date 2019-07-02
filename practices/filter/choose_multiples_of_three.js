c04ee0e9f8eda8184a5676f93d63944208bac1c4
Kong Fanyue <fykong@thoughtworks.com>
2019-07-02 12:00:04 +0800

    Finish choose three multiply number with filter array

--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function choose_multiples_of_three(collection) {
-
-  //implement here
+    return collection.filter(function (x) {
+      return x % 3 ==0;
+    });
 }
 
 module.exports = choose_multiples_of_three;
