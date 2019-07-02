c3c9102852945d051407ab0a52bad4cde6203a16
Kong Fanyue <fykong@thoughtworks.com>
2019-07-02 12:27:21 +0800

    Finish choose count map with map and object

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,8 +1,23 @@
 'use strict';
 
 function grouping_count(collection) {
+  var sum = new Map();
+  var obj = {};
+  
+  collection.forEach(element => {
+    var count = 1;
+    if(sum.has(element)){
+      count = sum.get(element) + 1;
+    }
+    
+    sum.set(element, count);
+  });
+  
+  sum.forEach(function (value, key, map) {
+    obj[key + ''] = value
+  });
 
-  //implement here
+  return obj;
 }
 
 module.exports = grouping_count;
